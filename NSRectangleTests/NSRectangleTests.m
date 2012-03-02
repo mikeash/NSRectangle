7bbedda1efcbd15e41f0af23e660f6851f7eb97c
Michael Ash <mikeash@MikeAir.local>
2012-03-01 19:23:45 -0500

    more comprehensive tests of MAPoint and MASize

--- NSRectangleTests/NSRectangleTests.m
+++ NSRectangleTests/NSRectangleTests.m
@@ -30,4 +30,69 @@
     STAssertEquals(s, [sobj sizeValueInCoordinateSystem: [[MACoordinateSystem alloc] initWithView: v]], @"Sizes are not equal");
 }
 
+- (void)testDifferentViewPoint
+{
+    NSPoint p = { 1, 2 };
+    
+    NSWindow *w = [[NSWindow alloc] initWithContentRect: NSMakeRect(0, 0, 100, 100) styleMask: 0 backing: NSBackingStoreBuffered defer: YES];
+    NSView *v = [[NSView alloc] initWithFrame: NSMakeRect(0, 0, 100, 100)];
+    NSView *v2 = [[NSView alloc] initWithFrame: NSMakeRect(50, 50, 100, 100)];
+    [[w contentView] addSubview: v];
+    [[w contentView] addSubview: v2];
+    
+    MAPoint *pobj = [MAPoint pointWithNSPoint: p coordinateSystem: [[MACoordinateSystem alloc] initWithView: v]];
+    
+    STAssertEquals(NSMakePoint(-49, -48), [pobj pointValueInCoordinateSystem: [[MACoordinateSystem alloc] initWithView: v2]], @"Points are not equal");
+}
+
+- (void)testDifferentViewSize
+{
+    NSSize s = { 1, 2 };
+    
+    NSWindow *w = [[NSWindow alloc] initWithContentRect: NSMakeRect(0, 0, 100, 100) styleMask: 0 backing: NSBackingStoreBuffered defer: YES];
+    NSView *v = [[NSView alloc] initWithFrame: NSMakeRect(0, 0, 100, 100)];
+    NSView *v2 = [[NSView alloc] initWithFrame: NSMakeRect(50, 50, 100, 100)];
+    [v2 scaleUnitSquareToSize: NSMakeSize(2, 2)];
+    [[w contentView] addSubview: v];
+    [[w contentView] addSubview: v2];
+    
+    MASize *sobj = [MASize sizeWithNSSize: s coordinateSystem: [[MACoordinateSystem alloc] initWithView: v]];
+    
+    STAssertEquals(NSMakeSize(0.5, 1), [sobj sizeValueInCoordinateSystem: [[MACoordinateSystem alloc] initWithView: v2]], @"Sizes are not equal");
+}
+
+- (void)testDifferentWindowPoint
+{
+    NSPoint p = { 1, 2 };
+    
+    NSWindow *w = [[NSWindow alloc] initWithContentRect: NSMakeRect(0, 0, 100, 100) styleMask: 0 backing: NSBackingStoreBuffered defer: YES];
+    NSView *v = [[NSView alloc] initWithFrame: NSMakeRect(0, 0, 100, 100)];
+    [[w contentView] addSubview: v];
+    NSWindow *w2 = [[NSWindow alloc] initWithContentRect: NSMakeRect(50, 50, 100, 100) styleMask: 0 backing: NSBackingStoreBuffered defer: YES];
+    NSView *v2 = [[NSView alloc] initWithFrame: NSMakeRect(0, 0, 100, 100)];
+    [[w2 contentView] addSubview: v2];
+    
+    MAPoint *pobj = [MAPoint pointWithNSPoint: p coordinateSystem: [[MACoordinateSystem alloc] initWithView: v]];
+    
+    STAssertEquals(NSMakePoint(-49, -48), [pobj pointValueInCoordinateSystem: [[MACoordinateSystem alloc] initWithView: v2]], @"Points are not equal");
+}
+
+- (void)testDifferentWindowSize
+{
+    NSSize s = { 1, 2 };
+    
+    NSWindow *w = [[NSWindow alloc] initWithContentRect: NSMakeRect(0, 0, 100, 100) styleMask: 0 backing: NSBackingStoreBuffered defer: YES];
+    NSView *v = [[NSView alloc] initWithFrame: NSMakeRect(0, 0, 100, 100)];
+    [[w contentView] addSubview: v];
+    
+    NSWindow *w2 = [[NSWindow alloc] initWithContentRect: NSMakeRect(50, 50, 100, 100) styleMask: 0 backing: NSBackingStoreBuffered defer: YES];
+    NSView *v2 = [[NSView alloc] initWithFrame: NSMakeRect(0, 0, 100, 100)];
+    [v2 scaleUnitSquareToSize: NSMakeSize(2, 2)];
+    [[w2 contentView] addSubview: v2];
+    
+    MASize *sobj = [MASize sizeWithNSSize: s coordinateSystem: [[MACoordinateSystem alloc] initWithView: v]];
+    
+    STAssertEquals(NSMakeSize(0.5, 1), [sobj sizeValueInCoordinateSystem: [[MACoordinateSystem alloc] initWithView: v2]], @"Sizes are not equal");
+}
+
 @end
