a63be49f37b338f4e7de3ce863b63996addd2ddc
Michael Ash <mikeash@MikeAir.local>
2012-03-02 21:12:44 -0500

    test some window/view manipulation in the UI

--- NSRectangle/MAAppDelegate.m
+++ NSRectangle/MAAppDelegate.m
@@ -8,13 +8,38 @@
 
 #import "MAAppDelegate.h"
 
-@implementation MAAppDelegate
+#import "NSView+MARectangleAdditions.h"
+#import "NSWindow+MARectangleAdditions.h"
+
+
+@interface TestView : NSView @end
+@implementation TestView
+
+- (void)drawRect: (NSRect)r
+{
+    [[NSColor redColor] setStroke];
+    [NSBezierPath strokeLineFromPoint: r.origin toPoint: NSMakePoint(NSMaxX(r), NSMaxY(r))];
+}
+
+@end
+
+@implementation MAAppDelegate {
+    NSWindow *w;
+}
 
 @synthesize window = _window;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    TestView *v = [[TestView alloc] init];
+    [[_window contentView] addSubview: v];
+    [v ma_setFrameRectangle: [[_window contentView] ma_frameRectangle]];
+    
+    w = [[NSWindow alloc] initWithContentMARectangle: [v ma_frameRectangle] styleMask: 0 backing: NSBackingStoreBuffered defer: YES];
+    TestView *v2 = [[TestView alloc] init];
+    [[w contentView] addSubview: v2];
+    [v2 ma_setFrameRectangle: [v ma_frameRectangle]];
+    [w orderFront: nil];
 }
 
 @end
